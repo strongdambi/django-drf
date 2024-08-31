332befc0c0ba82710a48dce17b955f9c7ac60831
strongdambi <thekeytohong@gmail.com>
2024-09-01 01:43:24 +0900

    STATIC_ROOT 추가

--- api_pjt/settings.py
+++ api_pjt/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "43.203.242.29",
     "localhost",
-    "127.0.0.1",
+    "127.0.0.1"
 ]
 
 
@@ -171,6 +171,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
